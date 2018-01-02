74b8c3369c1d8fe0818661c2b9c788c46cd324cb
Doğukan KURNAZ <dogukankurnaz@gmail.com>
2018-01-02 16:23:03 +0300

    Algoritma sınavı polinom sorusu

---
+++ Polinom.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+def Poli(x):
+    a =(5*(x**2))+(-7)*x+8 #örnek bir katsayı oluşumu
+    print a
+
+deger=int(raw_input("X değerini giriniz: ")) #Kullanıcıdan x değeri istedim.
+
+Poli(deger) # Kullanıcıdan aldığım değeri fonksiyonumuz da yerine koydu ve işlemi yaptı.
