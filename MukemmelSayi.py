123731fb5ecfd6e246bf7e7353020c1b8f82c2f9
Doğukan KURNAZ <dogukankurnaz@gmail.com>
2021-02-16 04:46:26 +0300

    Rename mükemmmmmmmmmel sayı to MukemmelSayi.py

--- MukemmelSayi.py
+++ MukemmelSayi.py
@@ -7,5 +7,5 @@ for a in range(2,1000):
     if t==a:
         x.append(a)
 
-print "2-1000 arasında",len(x),"adet mükemmel sayı var"
+print "2-1000 arasÃ½nda",len(x),"adet mÃ¼kemmel sayÃ½ var"
 print x
